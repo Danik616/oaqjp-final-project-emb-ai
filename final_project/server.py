5256eecceb83a28ef5438e3f1e80ddeadaaf7d6d
Danik616 <ddanielvargas2011@gmail.com>
2024-12-10 19:03:02 -0500

    som problem

--- final_project/server.py
+++ final_project/server.py
@@ -4,6 +4,6 @@ from EmotionDetection.emotion_detection import emotion_detector
 app = Flask(__name__)
 
 @app.route("/emotionDetector")
-def emotionDetector():
+def emotionDgmail.cometector():
     text = str(request.args.get('text'))
     result = emotion_detector()
